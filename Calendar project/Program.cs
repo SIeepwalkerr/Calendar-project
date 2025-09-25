0a16e9a2e1cb0d88ceb3bd805529e8412ba9abd3
Yuri Eliseev <eliseev_yura8@list.ru>
2025-09-25 18:08:32 +0300

    fixing fcking problems with UTF

--- Calendar project/Program.cs
+++ Calendar project/Program.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Text;
 public class Program : Form
 {
     private MonthCalendar monthCalendar1;
